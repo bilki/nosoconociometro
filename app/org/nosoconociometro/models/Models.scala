877f40fc54fcc059b1d07b6d28347dc9a27b1094
Roberto Serrano <roberto.serrano.marcos@bbva.com>
2017-06-09 19:36:31 +0200

    Primeros modelos para los cuestionarios y respuestas

---
+++ app/org/nosoconociometro/models/Models.scala
@@ -0,0 +1,25 @@
+package org.nosoconociometro.models
+
+import enumeratum._
+
+object Models {
+  case class Patient(name: String, age: Int)
+
+  sealed trait TipLevel extends EnumEntry
+  object TipLevel extends Enum[TipLevel] {
+    val values = findValues
+
+    case object TipWarning extends TipLevel
+    case object TipNeutral extends TipLevel
+    case object TipClear extends TipLevel
+  }
+
+  case class TipId(value: Int) extends AnyVal
+  case class TipResume(value: String) extends AnyVal
+  case class TipFull(value: String) extends AnyVal
+  case class Tip(id: TipId, tipResume: TipResume, tipFull: TipFull, tipLevel: TipLevel)
+
+  case class QuestionId(value: Int) extends AnyVal
+  case class QuestionText(value: Int) extends AnyVal
+  case class Question(id: QuestionId, text: QuestionText)
+}

45e340840b1a66df9e151a696702d09f268b2f30
patricio <patricio.fernandez.prieto@bbva.com>
2017-06-09 20:08:33 +0200

    Primera version del cuestionario

--- app/org/nosoconociometro/controllers/Application.scala
+++ app/org/nosoconociometro/controllers/Application.scala
@@ -1,10 +1,14 @@
 package org.nosoconociometro.controllers
 
+import javax.inject.Inject
+
+import controllers.WebJarAssets
+
 import play.api.mvc.{Action, Controller}
 
-class Application extends Controller {
+class Application @Inject()(webJar: WebJarAssets) extends Controller {
 
   def index = Action { implicit request =>
-    Ok(org.nosoconociometro.views.html.index())
+    Ok(org.nosoconociometro.views.html.index(webJar))
   }
 }
